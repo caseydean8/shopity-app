c5a52a9d75f4032b9ab66c60e88d20ae7b8c62a5
Nathan Hill <pjsstp@gmail.com>
2019-08-29 18:23:36 -0400

    updated to use var instead of const

--- config/express-config.js
+++ config/express-config.js
@@ -1,8 +1,8 @@
 // SET EXPRESS ENVIRONMENT
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
 
-var logger = require('./winston.js');
+// var logger = require('./winston.js');
 // var winston = require('winston'); // for transports.Console
 
 app.use(express.urlencoded({ extended: false }));
@@ -12,15 +12,15 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //  SET UP HANDLEBARS
-const expHandlebars = require('express-handlebars')
+var expHandlebars = require('express-handlebars')
 app.engine("handlebars", expHandlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // ADD IN WINSTON AS MIDDLEWARE LOGGER
-app.use(logger);
+// app.use(logger);
 
 // SET UP THE ROUTERS
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("../routes/apiRoutes")(app);
+require("../routes/htmlRoutes")(app);
 
 module.exports = app;
