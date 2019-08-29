03b2369216f2b288ec0965934b14fec0881ae7df
Nathan Hill <pjsstp@gmail.com>
2019-08-29 08:23:02 -0400

    added express-config.js to compartmentalize express server setup code including winston logger, and updated server.js to use express-config.js

---
+++ config/express-config.js
@@ -0,0 +1,26 @@
+// SET EXPRESS ENVIRONMENT
+const express = require('express')
+const app = express()
+
+var logger = require('./winston.js');
+// var winston = require('winston'); // for transports.Console
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// SERVE UP STATIC FILES
+app.use(express.static("public"));
+
+//  SET UP HANDLEBARS
+const expHandlebars = require('express-handlebars')
+app.engine("handlebars", expHandlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// ADD IN WINSTON AS MIDDLEWARE LOGGER
+app.use(logger);
+
+// SET UP THE ROUTERS
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,29 +1,8 @@
 require("dotenv").config();
-var express = require("express");
-var exphbs = require("express-handlebars");
+const app = require("./config/express-config")
+const db = require("./models");
 
-var db = require("./models");
-
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-// Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static("public"));
-
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
-
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+const PORT = process.env.PORT;
 
 var syncOptions = { force: false };
 
@@ -43,5 +22,3 @@ db.sequelize.sync(syncOptions).then(function() {
     );
   });
 });
-
-module.exports = app;
