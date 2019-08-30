17345d166624a7e30234a51eb4f1da8de9efddb2
Nathan Hill <pjsstp@gmail.com>
2019-08-30 09:39:08 -0400

    included winston logger to express-config.js and added winston.js for logger setup

--- config/express-config.js
+++ config/express-config.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const app = express();
 
+const logger = require("./winston.js");
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -12,6 +14,9 @@ app.use(express.static("public"));
 const expHandlebars = require("express-handlebars");
 app.engine("handlebars", expHandlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// ADD WINSTON LOGGER MIDDLEWARE TO SERVER
+app.use(logger);
 
 // SET UP THE ROUTERS
 require("../routes/apiRoutes")(app);
